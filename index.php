f02105733fd65698aa91f92c7dbb1fc32a3f84ba
Tim Jones <tim@timjones.id.au>
2018-06-19 14:29:39 +1000

    Added next steps
    Added section detailing next steps to take depending on the results of the test.
    
    Also added option to override real values (for testing).

--- index.php
+++ index.php
@@ -30,56 +30,134 @@
 <?PHP
 	echo "<pre>\n";
 	
-	echo "Running Golem port check\n";
-	echo "WAN IP detected as '$wanIP'\n";
-	echo "checking ports for IP '$checkIP'\n";
-	
-	$nodeID = 0;	
-	do{
-		$nodeID++;
-		$foundPort = false;
-		$failedPort = false;
-		$status = array(false, false, false);
-		$checkingPort = 0;
-		
-		echo "\nChecking ports for node number " . $nodeID . ":\n";
-		
-		while($checkingPort < sizeof($port))
-		{
-			$currentPort = $port[$checkingPort] + ($jump[$checkingPort] * ($nodeID - 1));
-			echo "Checking port " . $currentPort . ": ";
-			$stream = fsockopen($checkIP, $currentPort, $errno, $errstr, $timeout);
-			if (!$stream) {
-				echo "<font color='red'>{$errstr} (error {$errno})</font>\n";
-				$failedPort = true;
-			}
-			else
-			{
-				echo "<font color='green'>Success!</font>\n";
-				$foundPort = true;
-				$status[$checkingPort] = true;
-				fclose($stream);
-			}
-			$checkingPort++;
-		}
-		
-		if(!$failedPort)
-		{
-			$fullNodes++;
-		}
-		elseif($foundPort)
-		{
-			$partNodes++;
-		}
-		
-	} while ($foundPort);
-	
-	echo "\nChecked $nodeID nodes.\n";
-	echo "<font color='green'>Found $fullNodes fully open nodes.</font>\n";
-	echo "<font color='orange'>Found $partNodes partially open nodes.</font>\n";
+	if(mb_strlen($_GET['override']) == 2) // for testing purposes only
+	{
+	    $fullNodes = intval(substr($_GET['override'], 0, 1));
+	    $partNodes = intval(substr($_GET['override'], 1, 1));
+	    
+	    echo "\nOverriding with the following values:\n";
+	    echo "<font color='green'>Found $fullNodes fully open nodes.</font>\n";
+	    echo "<font color='orange'>Found $partNodes partially open nodes.</font>\n";
+	}
+	else
+	{
+	    echo "Running Golem port check\n";
+    	echo "WAN IP detected as '$wanIP'\n";
+    	echo "checking ports for IP '$checkIP'\n";
+    	
+    	$nodeID = 0;	
+    	do{
+    		$nodeID++;
+    		$foundPort = false;
+    		$failedPort = false;
+    		$status = array(false, false, false);
+    		$checkingPort = 0;
+    		
+    		echo "\nChecking ports for node number " . $nodeID . ":\n";
+    		
+    		while($checkingPort < sizeof($port))
+    		{
+    			$currentPort = $port[$checkingPort] + ($jump[$checkingPort] * ($nodeID - 1));
+    			echo "Checking port " . $currentPort . ": ";
+    			$stream = fsockopen($checkIP, $currentPort, $errno, $errstr, $timeout);
+    			if (!$stream) {
+    				echo "<font color='red'>{$errstr} (error {$errno})</font>\n";
+    				$failedPort = true;
+    			}
+    			else
+    			{
+    				echo "<font color='green'>Success!</font>\n";
+    				$foundPort = true;
+    				$status[$checkingPort] = true;
+    				fclose($stream);
+    			}
+    			$checkingPort++;
+    		}
+    		
+    		if(!$failedPort)
+    		{
+    			$fullNodes++;
+    		}
+    		elseif($foundPort)
+    		{
+    			$partNodes++;
+    		}
+    		
+    	} while ($foundPort);
+    	
+    	echo "\nChecked $nodeID nodes.\n";
+    	echo "<font color='green'>Found $fullNodes fully open nodes.</font>\n";
+    	echo "<font color='orange'>Found $partNodes partially open nodes.</font>\n";
+	}
 	
 	echo "\n</pre>\n";
+	
+	echo "<hr>\n";
+	echo "<b>Results & Next Steps:</b><br><br>\n";
+	
+	if($fullNodes < 1)
+	{
+	    if($partNodes < 1) // 0 fully open, 0 partly open
+	    {
+	        echo "The port check was not able to connect to any ports.<br>\n";
+	        echo "<ul><li>If you did not have Golem open when you ran this test, please open the Golem application, wait a few minutes, then <a href=''>run this test again</a>.</li>\n";
+	        echo "<li>If you did have Golem open when you ran this test, please make sure you have forwarded ports 3282, 40102, and 40103 in your router, as per the <a href='https://golem.network/documentation/09-common-issues-troubleshooting/port-forwarding-connection-errors/'>Golem Docs</a>.</li>\n";
+	        echo "<li>If you are certain that you have correctly forwarded these ports in your router, then something else is blocking these ports - for example Windows Firewall or other firewall software - or your ISP may not allow forwarding of specific custom ports.</li></ul>\n";
+	    }
+	    elseif($partNodes == 1) // 0 fully open, 1 partly open
+	    {
+	        echo "The port check was able to connect to some, but not all, required ports.<br>\n";
+	        echo "<ul><li>Please make sure you have correctly forwarded all 3 ports 3282, 40102, and 40103 in your router, as per the <a href='https://golem.network/documentation/09-common-issues-troubleshooting/port-forwarding-connection-errors/'>Golem Docs</a>, and double-check the ports that did not indicate a successful connection in the above test.</li>\n";
+	        echo "<li>If you are certain that you have correctly forwarded these ports in your router, then something else is blocking these ports - for example Windows Firewall or other firewall software - or your ISP may not allow forwarding of specific custom ports.</li></ul>\n";
+	    }
+	    else // 0 fully open, multiple partly open
+	    {
+	        echo "It appears you are trying to run multiple Golem nodes on your network, however none of these nodes have their ports correctly forwarded. The port check was able to connect to some, but not all, required ports.<br>\n";
+	        echo "<ul><li>Please make sure you have correctly forwarded all 3 ports for each node in your router, as per the <a href='https://golem.network/documentation/09-common-issues-troubleshooting/port-forwarding-connection-errors/'>Golem Docs</a>, and double-check the ports that did not indicate a successful connection in the above test.</li>\n";
+	        echo "<li>For an explanation of how to forward ports for multiple nodes running behind the one router/NAT setup, please see <a href='https://www.reddit.com/r/GolemProject/comments/8la5rk/golem_client_updates/dzfoprw/'>this example</a>.</li>\n";
+	        echo "<li>If you are certain that you have correctly forwarded these ports in your router, then something else is blocking these ports - for example Windows Firewall or other firewall software - or your ISP may not allow forwarding of specific custom ports.</li></ul>\n";
+	    }
+	}
+	elseif($fullNodes == 1)
+	{
+	    if($partNodes < 1) // 1 fully open, 0 partly open
+	    {
+	        echo "The port check found 1 fully open node. If you are trying to run Golem on a single computer on your network, then the ports are forwarded correctly for you. If you need any further assistance using Golem, please head to the <a href='https://chat.golem.network/channel/testers'>Golem chat</a><br><br>\n";
+	        echo "If you are attempting to run Golem on more than 1 computer, you will need to forward additional ports for the additional nodes.<br>\n";
+	        echo "<ul><li>If you did not have Golem open on all computers when you ran this test, please open the Golem application, wait a few minutes, then <a href=''>run this test again</a>.</li>\n";
+	        echo "<li>If you did have Golem open when you ran this test, please make sure you have correctly forwarded all 3 ports for each node in your router, as per the <a href='https://golem.network/documentation/09-common-issues-troubleshooting/port-forwarding-connection-errors/'>Golem Docs</a>, and double-check the ports that did not indicate a successful connection in the above test.</li>\n";
+	        echo "<li>For an explanation of how to forward ports for multiple nodes running behind the one router/NAT setup, please see <a href='https://www.reddit.com/r/GolemProject/comments/8la5rk/golem_client_updates/dzfoprw/'>this example</a>.</li>\n";
+	        echo "<li>If you are certain that you have correctly forwarded these ports in your router, then something else is blocking these ports - for example Windows Firewall or other firewall software - or your ISP may not allow forwarding of specific custom ports.</li></ul>\n";
+	    }
+	    else // 1 fully open, 1 or more partly open
+	    {
+	        echo "It appears you are trying to run multiple Golem nodes on your network, however only the first node has its ports correctly forwarded. The port check was able to connect to some, but not all, required ports.<br>\n";
+	        echo "<ul><li>Please make sure you have correctly forwarded all 3 ports for each node in your router, as per the <a href='https://golem.network/documentation/09-common-issues-troubleshooting/port-forwarding-connection-errors/'>Golem Docs</a>, and double-check the ports that did not indicate a successful connection in the above test.</li>\n";
+	        echo "<li>For an explanation of how to forward ports for multiple nodes running behind the one router/NAT setup, please see <a href='https://www.reddit.com/r/GolemProject/comments/8la5rk/golem_client_updates/dzfoprw/'>this example</a>.</li>\n";
+	        echo "<li>If you are certain that you have correctly forwarded these ports in your router, then something else is blocking these ports - for example Windows Firewall or other firewall software - or your ISP may not allow forwarding of specific custom ports.</li></ul>\n";
+	    }
+	}
+	else
+	{
+	    if($partNodes < 1) // multiple fully open, 0 partly open
+	    {
+	        echo "The port check found $fullNodes fully open nodes. If you are trying to run Golem on $fullNodes computers on your network, then the ports are forwarded correctly for you. If you need any further assistance using Golem, please head to the <a href='https://chat.golem.network/channel/testers'>Golem chat</a><br><br>\n";
+	        echo "If you are attempting to run Golem on more than $fullNodes computer, you will need to forward additional ports for the additional nodes.<br>\n";
+	        echo "<ul><li>If you did not have Golem open on all computers when you ran this test, please open the Golem application, wait a few minutes, then <a href=''>run this test again</a>.</li>\n";
+	        echo "<li>If you did have Golem open when you ran this test, please make sure you have correctly forwarded all 3 ports for each node in your router, as per the <a href='https://golem.network/documentation/09-common-issues-troubleshooting/port-forwarding-connection-errors/'>Golem Docs</a>, and double-check the ports that did not indicate a successful connection in the above test.</li>\n";
+	        echo "<li>For an explanation of how to forward ports for multiple nodes running behind the one router/NAT setup, please see <a href='https://www.reddit.com/r/GolemProject/comments/8la5rk/golem_client_updates/dzfoprw/'>this example</a>.</li>\n";
+	        echo "<li>If you are certain that you have correctly forwarded these ports in your router, then something else is blocking these ports - for example Windows Firewall or other firewall software - or your ISP may not allow forwarding of specific custom ports.</li></ul>\n";
+	    }
+	    else // multiple fully open, 1 or more partly open
+	    {
+	        echo "It appears you are trying to run multiple Golem nodes on your network, however only the first $fullNodes nodes have their ports correctly forwarded. The port check was able to connect to some, but not all, required ports.<br>\n";
+	        echo "<ul><li>Please make sure you have correctly forwarded all 3 ports for each node in your router, as per the <a href='https://golem.network/documentation/09-common-issues-troubleshooting/port-forwarding-connection-errors/'>Golem Docs</a>, and double-check the ports that did not indicate a successful connection in the above test.</li>\n";
+	        echo "<li>For an explanation of how to forward ports for multiple nodes running behind the one router/NAT setup, please see <a href='https://www.reddit.com/r/GolemProject/comments/8la5rk/golem_client_updates/dzfoprw/'>this example</a>.</li>\n";
+	        echo "<li>If you are certain that you have correctly forwarded these ports in your router, then something else is blocking these ports - for example Windows Firewall or other firewall software - or your ISP may not allow forwarding of specific custom ports.</li></ul>\n";
+	    }
+	}
 ?>
+        <br>
         <hr>
         <br>
         <a href='https://github.com/jet86/Golem-Port-Check-PHP'>View the source code</a><br>
